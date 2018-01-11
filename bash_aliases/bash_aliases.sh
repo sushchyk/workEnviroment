1b7307c39f193d57c3d61ccf4c0f34eae993c3c6
Andriy Sushchyk <andriy.sushchyk@sannacode.com>
2018-01-11 20:46:15 +0200

    Add new bash aliases

--- bash_aliases/bash_aliases.sh
+++ bash_aliases/bash_aliases.sh
@@ -1,3 +1,12 @@
+usephp() {
+    sudo update-alternatives --set php /usr/bin/php${1}
+    sudo a2dismod php5.6
+    sudo a2dismod php7.0
+    sudo a2dismod php7.1
+    sudo a2enmod php${1}
+    sudo service apache2 restart
+}
+
 pu() {
    if [ -f vendor/bin/phpunit ]; then
      vendor/bin/phpunit "$@"
@@ -19,7 +28,8 @@ alias c="composer $@"
 alias pa="php artisan $@"
 alias pat="php artisan tinker"
 alias pamt="php artisan make:test $1"
+alias lcr="echo > storage/logs/laravel.log"
 
 alias gs="git status"
 alias gc="git commit -m $@"
-alias nah="git reset * HEAD hard; git clean -fd"
+alias nah="git reset * HEAD --hard && git clean -fd"

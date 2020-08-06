847b600ec04c3daf6cb52028d5dde618537db8a0
Andriy Sushchyk <sushchyk12@gmail.com>
2020-08-06 18:29:07 +0300

    Save

--- bash_aliases/bash_aliases.sh
+++ bash_aliases/bash_aliases.sh
@@ -1,3 +1,5 @@
+alias ping_aliases="echo PONG"
+
 usephp() {
     sudo update-alternatives --set php /usr/bin/php${1} \
     && sudo a2dismod php5.6 \
@@ -6,32 +8,3 @@ usephp() {
     && sudo a2enmod php${1} \
     && sudo service apache2 restart
 }
-
-pu() {
-   if [ -f vendor/bin/phpunit ]; then
-     vendor/bin/phpunit "$@"
-   else
-     phpunit "$@"
-   fi
-}
-
-puf() {
-   if [ -f vendor/bin/phpunit ]; then
-     vendor/bin/phpunit --filter="$@"
-   else
-     phpunit --filter="$@"
-   fi
-}
-
-alias c="composer $@"
-
-alias pa="php artisan $@"
-alias pat="php artisan tinker"
-alias pamt="php artisan make:test $1"
-alias lcr="echo > storage/logs/laravel.log"
-
-alias gs="git status"
-alias gc="git commit -m $@"
-alias nah="git reset HEAD --hard && git clean -fd"
-
-alias bc="php bin/console $@"
